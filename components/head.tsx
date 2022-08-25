f7e8e9a21508c720dc4306b2b6b85b1f3213eee5
Oein <oein0219@gmail.com>
2022-08-25 16:52:54 +0900

    add auth , head

--- components/head.tsx
+++ components/head.tsx
@@ -1,8 +1,11 @@
 import { Grid, useTheme } from "@nextui-org/react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function MyHead() {
   let { theme } = useTheme();
+  const { data: session, status } = useSession();
+
   return (
     <div
       style={{
@@ -17,19 +20,49 @@ export default function MyHead() {
         borderBottom: `1px solid ${theme?.colors.neutralBorder}`,
       }}
     >
-      <Grid.Container>
-        <Grid>
-          <Link href="/">
-            <h1
+      <Link href="/" style={{}}>
+        <h1
+          style={{
+            cursor: "pointer",
+          }}
+        >
+          Dioh
+        </h1>
+      </Link>
+      <div
+        style={{
+          display: "inline-block",
+          position: "absolute",
+          right: "10px",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+      >
+        {status == "authenticated" ? session.user?.name + " " : ""}
+        {status == "authenticated" ? (
+          <Link href="/api/auth/signout">
+            <div
+              style={{
+                display: "inline-block",
+                cursor: "pointer",
+              }}
+            >
+              {" "}
+              | Sign Out
+            </div>
+          </Link>
+        ) : (
+          <Link href="/api/auth/signin">
+            <div
               style={{
                 cursor: "pointer",
               }}
             >
-              Dioh
-            </h1>
+              Sign In
+            </div>
           </Link>
-        </Grid>
-      </Grid.Container>
+        )}
+      </div>
     </div>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { createTheme } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 
 const lightTheme = createTheme({
   type: "dark",
@@ -16,18 +17,20 @@ const darkTheme = createTheme({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextThemesProvider
-      defaultTheme="system"
-      attribute="class"
-      value={{
-        light: lightTheme.className,
-        dark: darkTheme.className,
-      }}
-    >
-      <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>
-    </NextThemesProvider>
+    <SessionProvider session={pageProps.session}>
+      <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className,
+        }}
+      >
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </NextThemesProvider>
+    </SessionProvider>
   );
 }
 
