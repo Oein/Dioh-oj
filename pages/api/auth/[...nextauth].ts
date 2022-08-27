8080c55820bb1a1a3543252b9484e9fc57eac893
Oein <oein0219@gmail.com>
2022-08-27 16:56:36 +0900

    Add Naver Login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,28 @@
-import NextAuth, { Account, Profile, Session, User } from "next-auth";
+import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import NaverProvider from "next-auth/providers/naver";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import { JWT } from "next-auth/jwt";
 
 const prisma = new PrismaClient();
+
+const lpad = function (padString: string, length: number) {
+  var str = padString;
+  while (str.length < length) str = "0" + str;
+  return str;
+};
+
+function getUsername(name: string, email: string, id: string) {
+  let x = 0;
+  for (let i = 0; i < (email as string).length; i++) {
+    x += (email as string).charCodeAt(i);
+  }
+  let idonlynum = Math.floor(
+    parseInt((id as string).replace(/\D/g, "") as string) * 1.25 * 3 + x
+  );
+  let a = name;
+  return `${a}#${lpad((idonlynum % 10000).toString(), 4)}`;
+}
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -13,18 +31,24 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
+    NaverProvider({
+      clientId: process.env.NAVER_ID as string,
+      clientSecret: process.env.NAVER_SECRET as string,
+    }),
   ],
   callbacks: {
     session: async ({ session, token }) => {
       if (session?.user) {
         session.user.id = token.uid;
       }
+
       return session;
     },
     jwt: async ({ user, token }) => {
       if (user) {
         token.uid = user.id;
       }
+
       return token;
     },
   },
@@ -33,5 +57,28 @@ export default NextAuth({
   },
   pages: {
     signIn: "/auth/signin",
+  },
+  events: {
+    createUser: async ({ user }) => {
+      await prisma.user
+        .update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            nickName: getUsername(
+              user.name || "User",
+              user.email || "",
+              user.id
+            ),
+          },
+        })
+        .then(() => {
+          Promise.resolve();
+        })
+        .catch(() => {
+          console.log("ERR");
+        });
+    },
   },
 });
