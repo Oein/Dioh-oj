df938b8d38fc5214a450c11425be44bde7633cd6
Oein <oein0219@gmail.com>
2022-08-27 23:47:46 +0900

    Add shortcut on main page

--- components/MainPage/shortcuts.tsx
+++ components/MainPage/shortcuts.tsx
@@ -33,6 +33,34 @@ export default function ShortCuts() {
             </Link>
           </Grid>
         </Grid.Container>
+        <Grid.Container>
+          <Grid>
+            <Image
+              showSkeleton
+              width={"2em"}
+              objectFit="contain"
+              src="../../images/bus.svg"
+              alt="Dina의 이미지"
+              style={{
+                cursor: "pointer",
+              }}
+            />
+          </Grid>
+          <Grid>
+            <Link href="https://beta.jungol.co.kr/problem/1193">
+              <div
+                style={{
+                  color: "var(--nextui-colors-text)",
+                  fontSize: "var(--nextui-fontSizes-xl)",
+                  cursor: "pointer",
+                }}
+                className="font centerH"
+              >
+                메타행 버스타기
+              </div>
+            </Link>
+          </Grid>
+        </Grid.Container>
       </div>
     </>
   );
