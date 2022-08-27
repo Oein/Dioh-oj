926eeca51f7e1a6a340e2ed77dab043a803fd531
Oein <oein0219@gmail.com>
2022-08-27 16:57:25 +0900

    Add solved Count , solve requested Count & problem's point

--- pages/problem/[idx].tsx
+++ pages/problem/[idx].tsx
@@ -13,6 +13,7 @@ import rehypeKatex from "rehype-katex";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import docco from "react-syntax-highlighter/dist/cjs/styles/hljs/docco";
 import { toast } from "react-toastify";
+import { HeadingComponent } from "react-markdown/lib/ast-to-react";
 
 export default function ProblemPage() {
   let router = useRouter();
@@ -20,6 +21,9 @@ export default function ProblemPage() {
   let [problemArticles, setProblemArticles] = useState("Loading...");
   let [problemMemory, setProblemMemory] = useState("Loading...");
   let [problemTime, setProblemTime] = useState("Loading...");
+  let [problemPoint, setProblemPoint] = useState("Loading...");
+  let [problemReqs, setProblemReqs] = useState("Loading...");
+  let [problemSucs, setProblemSucs] = useState("Loading...");
 
   // 1번만 실행
   useEffect(() => {
@@ -40,9 +44,20 @@ export default function ProblemPage() {
       setProblemName(`${problem.id} : ${problem.name}`);
       setProblemMemory(problem.maxMemoryMB + "MB");
       setProblemTime(problem.maxTime + "ms");
+      setProblemPoint(problem.point);
+      setProblemReqs(problem.solveRequestedCount + "번");
+      setProblemSucs(problem.solvedPeopleCount + "명");
       setProblemArticles(problem.body);
     });
   }, [router.isReady, router.query]);
+
+  const markdownH3: HeadingComponent = ({ children, ...props }) => {
+    return (
+      <div className="borderBottom">
+        <h3 className="borderBottomColored">{children}</h3>
+      </div>
+    );
+  };
 
   return (
     <article className="container">
@@ -76,11 +91,17 @@ export default function ProblemPage() {
         <Table.Header>
           <Table.Column>제한 시간</Table.Column>
           <Table.Column>메모리 제한</Table.Column>
+          <Table.Column>포인트</Table.Column>
+          <Table.Column>제출 횟수</Table.Column>
+          <Table.Column>성공한 사람수</Table.Column>
         </Table.Header>
         <Table.Body>
           <Table.Row>
             <Table.Cell>{problemTime}</Table.Cell>
             <Table.Cell>{problemMemory}</Table.Cell>
+            <Table.Cell>{problemPoint}</Table.Cell>
+            <Table.Cell>{problemReqs}</Table.Cell>
+            <Table.Cell>{problemSucs}</Table.Cell>
           </Table.Row>
         </Table.Body>
       </Table>
@@ -120,13 +141,9 @@ export default function ProblemPage() {
                 </code>
               );
             },
-            h2({ children, ...props }) {
-              return (
-                <div className="borderBottom">
-                  <h2 className="borderBottomColored">{children}</h2>
-                </div>
-              );
-            },
+            h2: markdownH3,
+            h1: markdownH3,
+            h3: markdownH3,
           }}
         >
           {problemArticles}
@@ -135,16 +152,6 @@ export default function ProblemPage() {
 
       {/* css */}
       <style jsx>{`
-        .borderBottom {
-          border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-        }
-
-        .borderBottomColored {
-          border-bottom: 2px solid var(--nextui-colors-primary);
-          display: inline-block;
-          margin: 0 0 -2px 0;
-        }
-
         .copyToClipboardPar {
           position: relative;
         }
@@ -159,6 +166,18 @@ export default function ProblemPage() {
 
         .copyToClipboard:hover {
           transform: scale(1.5);
+        }
+      `}</style>
+
+      <style>{`
+        .borderBottom {
+          border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+        }
+
+        .borderBottomColored {
+          border-bottom: 2px solid var(--nextui-colors-primary);
+          display: inline-block;
+          margin: 0 0 -2px 0;
         }
       `}</style>
     </article>
