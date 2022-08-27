e43dbb994832eedf47a205c775de91bc0495dd38
Oein <oein0219@gmail.com>
2022-08-27 14:01:49 +0900

    Center Align main page

--- components/MainPage/main.tsx
+++ components/MainPage/main.tsx
@@ -1,40 +1,47 @@
 import { Grid, Image } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Main() {
   return (
     <>
-      <div
-        style={{
-          fontSize: "var(--nextui-fontSizes-xl4)",
-        }}
-      >
-        새로 추가된 문제
-      </div>
-      <Grid.Container>
-        <Grid>
-          <Image
-            showSkeleton
-            width={"2em"}
-            objectFit="contain"
-            src="/images/articles.svg"
-            alt="Dina의 이미지"
-          />
-        </Grid>
-        <Grid>
-          <div
-            style={{
-              position: "relative",
-              top: "50%",
-              margin: "0px",
-              padding: "0px",
-              transform: "translate(10px , -50%)",
-              fontSize: "var(--nextui-fontSizes-xl)",
-            }}
-          >
-            <strong>1000 : A+B</strong>
-          </div>
-        </Grid>
-      </Grid.Container>
+      <article className="container">
+        <div
+          style={{
+            fontSize: "var(--nextui-fontSizes-xl4)",
+          }}
+        >
+          문제 리스트
+        </div>
+
+        <Link href="/problem/1000">
+          <Grid.Container>
+            <Grid>
+              <Image
+                showSkeleton
+                width={"2em"}
+                objectFit="contain"
+                src="/images/articles.svg"
+                alt="Dina의 이미지"
+              />
+            </Grid>
+            <Grid>
+              <div
+                style={{
+                  position: "relative",
+                  top: "50%",
+                  margin: "0px",
+                  padding: "0px",
+                  transform: "translate(10px , -50%)",
+                  fontSize: "var(--nextui-fontSizes-xl)",
+                  cursor: "pointer",
+                }}
+              >
+                <strong>1000 : A+B</strong>
+              </div>
+            </Grid>
+          </Grid.Container>
+        </Link>
+      </article>
     </>
   );
 }
