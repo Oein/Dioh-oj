f9c264324501c03ed3ba79ef4e9da700e298be7d
Oein <oein0219@gmail.com>
2022-08-27 20:11:01 +0900

    Change main page

--- components/MainPage/main.tsx
+++ components/MainPage/main.tsx
@@ -1,46 +1,40 @@
-import { Grid, Image } from "@nextui-org/react";
-import Link from "next/link";
-
 export default function Main() {
   return (
     <>
       <article className="container">
-        <div
+        <header
           style={{
-            fontSize: "var(--nextui-fontSizes-xl4)",
+            borderBottom: "1px solid var(--nextui-colors-neutralBorder)",
+            paddingTop: "32px",
+            paddingBottom: "32px",
           }}
         >
-          문제 리스트
-        </div>
-
-        <Link href="/problem/1000">
-          <Grid.Container>
-            <Grid>
-              <Image
-                showSkeleton
-                width={"2em"}
-                objectFit="contain"
-                src="/images/articles.svg"
-                alt="Dina의 이미지"
-              />
-            </Grid>
-            <Grid>
-              <div
-                style={{
-                  position: "relative",
-                  top: "50%",
-                  margin: "0px",
-                  padding: "0px",
-                  transform: "translate(10px , -50%)",
-                  fontSize: "var(--nextui-fontSizes-xl)",
-                  cursor: "pointer",
-                }}
-              >
-                <strong>1000 : A+B</strong>
-              </div>
-            </Grid>
-          </Grid.Container>
-        </Link>
+          <div
+            style={{
+              fontSize: "var(--nextui-fontSizes-xl3)",
+              textAlign: "center",
+            }}
+          >
+            <div
+              style={{
+                borderBottom: "1px solid black",
+                width: "fit-content",
+                position: "relative",
+                left: "50%",
+                transform: "translateX(-50%)",
+              }}
+            >
+              Dioh / 디오
+            </div>
+            <div
+              style={{
+                fontSize: "var(--nextui-fontSizes-md)",
+              }}
+            >
+              DIna and Oein&apos;s online judge
+            </div>
+          </div>
+        </header>
       </article>
     </>
   );
