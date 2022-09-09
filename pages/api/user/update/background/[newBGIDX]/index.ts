3e11715c4f304125ff312b1b573e72f18993fb15
Oein <oein0219@gmail.com>
2022-09-10 00:50:25 +0900

    profile background change api

---
+++ pages/api/user/update/background/[newBGIDX]/index.ts
@@ -0,0 +1,79 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../../../../../util/prisma";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return new Promise<void>(async (resolve, reject) => {
+    let newBGIDX = req.query.newBGIDX;
+    let uToken = req.headers.authorization as string;
+
+    if (uToken == "") {
+      res.send(
+        JSON.stringify({
+          err: "Not logged in",
+        })
+      );
+      resolve();
+      return;
+    }
+    let token = await prisma.user.findFirst({
+      where: {
+        id: uToken,
+      },
+    });
+    if (token == null) {
+      res.send(
+        JSON.stringify({
+          err: "Not logged in",
+        })
+      );
+      resolve();
+      return;
+    }
+    if ((token.permission as string[]).includes("not_submit")) {
+      res.send(
+        JSON.stringify({
+          err: "Not has permission to submit",
+        })
+      );
+      resolve();
+      return;
+    }
+    if ((token.permission as string[]).includes("banned")) {
+      res.send(
+        JSON.stringify({
+          err: "Banned from the server",
+        })
+      );
+      resolve();
+      return;
+    }
+
+    await prisma.user
+      .update({
+        where: {
+          id: uToken,
+        },
+        data: {
+          UserBackgroundImgIndex: parseInt(newBGIDX as string),
+        },
+      })
+      .then((v) => {
+        res.send(
+          JSON.stringify({
+            suc: "Successfully updated",
+          })
+        );
+        resolve();
+      })
+      .catch((err) => {
+        res.send(
+          JSON.stringify({
+            err: err.message,
+          })
+        );
+        resolve();
+        return;
+      });
+  });
+}
