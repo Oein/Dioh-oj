604f4143b0e1b1aeb81f7e6a538f30a9842cd924
Oein <oein0219@gmail.com>
2022-09-09 21:32:23 +0900

    export bg urls

---
+++ util/backgroundImgURLS.ts
@@ -0,0 +1,6 @@
+const backgroundImgURLS = [
+  "https://cdn.discordapp.com/attachments/1017382361618456587/1017603918789885982/unknown.png",
+  "/backgrounds/1.svg",
+];
+
+export default backgroundImgURLS;
