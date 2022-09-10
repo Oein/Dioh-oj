9e58c72cb5c45960506f61f0fcaea20a582156a2
Oein <oein0219@gmail.com>
2022-09-10 11:44:14 +0900

    New Background

--- util/backgroundImgURLS.ts
+++ util/backgroundImgURLS.ts
@@ -1,8 +1,9 @@
 const backgroundImgURLS = [
   "https://cdn.discordapp.com/attachments/1017382361618456587/1017603918789885982/unknown.png",
   "/backgrounds/1.svg",
+  "/backgrounds/2.svg",
 ];
 
 export default backgroundImgURLS;
-export const backgroundNames = ["기본 배경", "와일드뀨라임"];
+export const backgroundNames = ["기본 배경", "와일드뀨라임", "XP"];
 export const ignoreIDX = [0];
