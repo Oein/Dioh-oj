5ce4c4e8305197ad09fe0cf18ee04d50b956093d
Oein <oein0219@gmail.com>
2022-09-14 18:06:03 +0900

    version name

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -40,7 +40,7 @@ export default function Footer() {
                     color: "var(--nextui-colors-accents7)",
                   }}
                 >
-                  (v.1.19.0)
+                  (v.1.19.0-1)
                 </Text>
               </Grid>
             </Grid.Container>
