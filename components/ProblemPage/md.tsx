e0533dfdba149b924304b6ddbab42a2ab46ff899
Oein <oein0219@gmail.com>
2022-09-14 18:04:52 +0900

    remove space bar icon

--- components/ProblemPage/md.tsx
+++ components/ProblemPage/md.tsx
@@ -73,7 +73,6 @@ export default function MD(props: { text: string }) {
                     ? ("_EMEMEM_" + String(children) + "_EMEMEM_")
                         .replace("_EMEMEM_", "_X_X_X_X_")
                         .replace(/\n$/, "")
-                        .replaceAll(" ", "␣")
                         .replaceAll("\n_EMEMEM_", "_EMEMEM_")
                         .replaceAll("\n", "↵\n")
                         .replaceAll("_X_X_X_X_", "\n")
